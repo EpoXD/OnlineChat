2d8366f9cdc90203e75b2fc2e8fce45a66fcf47d
Epo <pavlomazur20@gmail.com>
2023-02-16 18:24:03 +0100

    first commit

---
+++ chatroom/public/code.js
@@ -0,0 +1,97 @@
+
+
+    const app = document.querySelector(".app")
+    const socket = io();
+
+    let uname;
+
+    app.querySelector(".join-screen #join-user").addEventListener("click", function(){
+        let username = app.querySelector(".join-screen #username").value;
+        if (username.length == 0){
+            return
+        }
+        socket.emit("newuser", username);
+        uname = username;
+        app.querySelector(".join-screen").classList.remove("active");
+        app.querySelector(".chat-screen").classList.add("active");
+    });
+
+    app.querySelector(".chat-screen #send-message").addEventListener("click", function(){
+        let message = app.querySelector(".chat-screen #message-input").value;
+        if(message.length == 0){
+            return;
+        }
+        renderMessage("my",{
+            username:uname,
+            text:message
+        });
+        socket.emit("chat",{
+            username:uname,
+            text:message
+        });
+        app.querySelector(".chat-screen #message-input").value = "";
+    });
+
+    app.querySelector(".chat-screen #message-input").addEventListener("keydown", (event) => {
+        if (event.key === "Enter"){
+        let message = app.querySelector(".chat-screen #message-input").value;
+        if(message.length == 0){
+            return;
+        }
+        renderMessage("my",{
+            username:uname,
+            text:message
+        });
+        socket.emit("chat",{
+            username:uname,
+            text:message
+        });
+        app.querySelector(".chat-screen #message-input").value = "";
+        }
+    });
+
+
+
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click",function(){
+        socket.emit("exituser",uname);
+        window.location.href = window.location.href
+    });
+
+    socket.on("update",function(update){
+        renderMessage("update",update);
+    });
+    socket.on("chat",function(message){
+        renderMessage("other",message);
+    });
+
+    function renderMessage(type,message){
+        let messageContainer = app.querySelector(".chat-screen .messages");
+        if(type == "my"){
+            let el = document.createElement("div");
+            el.setAttribute("class","message my-message");
+            el.innerHTML = `
+                <div>
+                    <a href=""" class="name" id="my-profile">You</a>
+                    <div class="text">${message.text}</div>
+                </div>
+                <img class="my-image" src="cat.png">`
+                messageContainer.appendChild(el);
+            } else if(type == "other"){
+            let el = document.createElement("div");
+            el.setAttribute("class","message other-message");
+            el.innerHTML = `
+                <img class="other-image" src="cat.png">
+                <div>
+                    <div class="name">${message.username}</div>
+                    <div class="text">${message.text}</div>
+                </div>`
+                messageContainer.appendChild(el);
+        } else if(type == "update"){
+            let el = document.createElement("div");
+            el.setAttribute("class","update");
+            el.innerHTML = message
+            messageContainer.appendChild(el);
+        }
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+        
+    }
